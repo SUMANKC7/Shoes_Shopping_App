0b8ca5a43206c3df9f5740b6706474058bcb9d7f
SUMANKC7 <sumankc844@gmail.com>
2024-03-18 22:31:24 +0545

    HomePage Shoes listing is done

---
+++ lib/global_variable.dart
@@ -0,0 +1,51 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.52,
+    'imageUrl': 'assests/images/shoes_3.png',
+    'company': 'Nike',
+    'sizes': [9, 10, 11, 12],
+  },
+  {
+    'id': '1',
+    'title': 'Addidas Shoes',
+    'price': 20.12,
+    'imageUrl': 'assests/images/shoes_1.png',
+    'company': 'Addidas',
+    'sizes': [9, 10, 12],
+  },
+  {
+    'id': '2',
+    'title': 'Goldstar Men\'s Shoes',
+    'price': 28.95,
+    'imageUrl': 'assests/images/shoes_2.png',
+    'company': 'Goldstar',
+    'sizes': [8, 9, 10],
+  },
+  {
+    'id': '3',
+    'title': 'AirJordan Shoes',
+    'price': 420.69,
+    'imageUrl': 'assests/images/shoes_4.png',
+    'company': 'Nike',
+    'sizes': [8, 9, 10],
+  },
+
+  {
+    'id': '4',
+    'title': 'Jordan Shoes',
+    'price': 520.69,
+    'imageUrl': 'assests/images/images.jpg',
+    'company': 'Nike',
+    'sizes': [8, 9, 10],
+  },
+];
+final cart = [{
+  'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.69,
+    'imageUrl': 'assests/images/shoes_4.png',
+    'company': 'Nike',
+    'sizes': [8, 9, 10],
+}];
